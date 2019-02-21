fc574eed4b799852e25dc37a7e3759623724574a
Sarah Katz <soshasarah@gmail.com>
2019-02-21 16:00:16 -0500

    updates to latest gatsby version' '

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <div>
+  <Layout>
     <h1>NOT FOUND</h1>
     <p>Here I am, brain the size of a planet, and I can't find this page.</p>
-  </div>
+  </Layout>
 )
 
 export default NotFoundPage
---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => (
+  <Layout>
+    <h5>The Blog posts will go here</h5>
+  </Layout>
+)
+
+export default Blog
