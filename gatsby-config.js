f488818eb195a25a3d445d30501f0ec22f5479d6
Sarah Katz <soshasarah@gmail.com>
2019-02-28 21:00:16 -0500

    add netlify header package

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`
   ],
 }
