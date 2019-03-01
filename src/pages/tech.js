a68a86c6e5ea83898f70d7df2e23a2d72347424a
Sarah Katz <soshasarah@gmail.com>
2019-02-28 21:37:20 -0500

    changes to fix styling issues caused by other pages

--- src/pages/tech.js
+++ src/pages/tech.js
@@ -7,29 +7,29 @@ const TechStack = () => (
   <Layout>
     <div>
       <h3>I Can Work With...</h3>
-      <ul>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Next.js</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>Jasmine</li>
-        <li>Mocha</li>
-        <li>Chai</li>
-        <li>Selenium Webdriver</li>
-        <li>Jest</li>
+      <ul className="tech-list">
+        <li className="tech-item">JavaScript</li>
+        <li className="tech-item">HTML</li>
+        <li className="tech-item">CSS</li>
+        <li className="tech-item">React</li>
+        <li className="tech-item">Redux</li>
+        <li className="tech-item">Next.js</li>
+        <li className="tech-item">Node.js</li>
+        <li className="tech-item">Express</li>
+        <li className="tech-item">PostgreSQL</li>
+        <li className="tech-item">Sequelize</li>
+        <li className="tech-item">Jasmine</li>
+        <li className="tech-item">Mocha</li>
+        <li className="tech-item">Chai</li>
+        <li className="tech-item">Selenium Webdriver</li>
+        <li className="tech-item">Jest</li>
       </ul>
     </div>
     <div>
       <h3>I'm Still Learning...</h3>
-      <ul>
-        <li>Gatsby</li>
-        <li>GraphQL</li>
+      <ul className="tech-list">
+        <li className="tech-item">Gatsby</li>
+        <li className="tech-item">GraphQL</li>
       </ul>
     </div>
   </Layout>
