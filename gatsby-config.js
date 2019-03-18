a215d4bae7958f22c4ac65a7f44b2cfc21797617
Sarah Katz <soshasarah@gmail.com>
2019-03-17 20:48:04 -0400

    Blog 3/17 (#18)
    * add netlify header package
    
    * Blog/03 02 19 (#9)
    
    * changes to fix styling issues caused by other pages
    
    * march 3rd blog
    
    * Blog/03 02 19 (#11)
    
    * changes to fix styling issues caused by other pages
    
    * march 3rd blog
    
    * styling changes
    
    * Projects page (#12)
    
    * adds project components and all projects
    
    * style changes and add to front page
    
    * adds downloadable resume (#13)
    
    * update project image
    
    * fixes picture issue on projects
    
    * Blog/03 10 (#15)
    
    * adds disqus comment section
    
    * adds 3-10 blog
    
    * Blog/03 17 (#17)
    
    * adds rss feed for blog
    
    * blog for march 17th
    
    * Set draft to false
    
    * Typo fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Sarah Katz',
+    siteUrl: 'https://www.sarahlkatz.com'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -37,6 +38,61 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-netlify`
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: {frontmatter: { draft: { ne: true } }}
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                        path
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Sarah L. Katz Blog",
+          },
+        ],
+      },
+    },
   ],
 }
--- src/components/social.js
+++ src/components/social.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import github from '../images/github.png'
 import linkedin from '../images/linkedin.png'
 import twitter from '../images/twitter.png'
 import medium from '../images/medium.png'
+import rss from '../images/rss.svg'
 
 import '../styles/layout.css'
 
@@ -22,6 +24,9 @@ const Social = () => (
       <a href="https://twitter.com/sarahscode">
         <img src={twitter} alt="twitter @sarahscode" className="socialImage" />
       </a>
+      <Link to="rss.xml">
+        <img src={rss} alt="blog rss subscription" className="socialImage" />
+      </Link>
       <a href="https://medium.com/@sarahscode">
         <img src={medium} alt="medium @sarahscode" className="socialImage" />
       </a>
