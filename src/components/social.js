1b50af29147d710e0546aff82138257c7c1c4d9f
Sarah Katz <soshasarah@gmail.com>
2019-06-26 14:58:50 -0400

    small change to rss feed link

--- src/components/social.js
+++ src/components/social.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import github from '../images/github.png'
 import linkedin from '../images/linkedin.png'
 import twitter from '../images/twitter.png'
@@ -30,9 +29,9 @@ const Social = () => (
           className="socialImage"
         />
       </a>
-      <Link to="rss.xml">
+      <a href="rss.xml">
         <img src={rss} alt="blog rss subscription" className="socialImage" />
-      </Link>
+      </a>
       <a href="https://medium.com/@sarahscode">
         <img src={medium} alt="medium @sarahscode" className="socialImage" />
       </a>
