e52c4572116b918bd9d0efc27dde7f232d1c96b8
Sarah Katz <soshasarah@gmail.com>
2019-10-07 09:32:13 -0400

    blog for 10/7 (#81) (#82)
    * blog for 10/7 (#81)
    
    * blog for 10/7
    
    * rewrite after re-read
    
    * 404 page fix - divider line

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,24 +2,29 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
+import '../styles/404.css'
+
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>
-      Oops! It looks like you've found a page that doesn't exist. I'm guessing
-      this was not what you intended. But here's some good news:{' '}
-      <Link to="/">
-        you can click here to return to the homepage (or use the navigation
-        above to go to a different page)!
-      </Link>
-    </p>
-    <p>
-      If you have a few minutes, I'd love if you could tweet at me at{' '}
-      <a href="http://www.twitter.com/sarahscode">@sarahscode</a> (or DM me if
-      you prefer) to let me know what you were trying to do that led you to this
-      page so that I can investigate further and see if I can fix some broken
-      links. Thank you!!
-    </p>
+    <div className="err">
+      <hr />
+      <h1>NOT FOUND</h1>
+      <p>
+        Oops! It looks like you've found a page that doesn't exist. I'm guessing
+        this was not what you intended. But here's some good news:{' '}
+        <Link to="/">
+          you can click here to return to the homepage (or use the navigation
+          above to go to a different page)!
+        </Link>
+      </p>
+      <p>
+        If you have a few minutes, I'd love if you could tweet at me at{' '}
+        <a href="http://www.twitter.com/sarahscode">@sarahscode</a> (or DM me if
+        you prefer) to let me know what you were trying to do that led you to this
+        page so that I can investigate further and see if I can fix some broken
+        links. Thank you!!
+      </p>
+    </div>
   </Layout>
 )
 
