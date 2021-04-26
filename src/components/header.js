49ecd19925e906a871d3c05e5789deb9b5dd3a5f
Sarah Katz <soshasarah@gmail.com>
2021-04-25 21:51:58 -0400

    Oops home

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 import '../styles/layout.css'
 
 const Header = ({ siteTitle }) => (
   <div className="titleContent">
-    <h1 className="title">
+    <Link key="home" to="/">{name}<h1 className="title">
       {siteTitle}
-    </h1>
+    </h1></Link>
     <div className="subtitle">
       NYC-Based Full Stack JS Developer
     </div>
